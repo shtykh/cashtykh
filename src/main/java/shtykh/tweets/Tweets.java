5a7b60cdd569abd3a056286fb236b4d7569ada33
Alexey Shtykh <shtykh.alexey@gmail.com>
2015-02-11 01:39:09 +0300

    minor fix

--- src/main/java/shtykh/tweets/Tweets.java
+++ src/main/java/shtykh/tweets/Tweets.java
@@ -15,7 +15,18 @@ public class Tweets extends Story {
 
 	private static String getBody(String content) throws JSONException {
 		JSONObject jsonObject = new JSONObject(content);
-		JSONArray statuses = jsonObject.getJSONArray("statuses");
+		JSONArray statuses;
+		try {
+			statuses = jsonObject.getJSONArray("statuses");
+		} catch (JSONException couldNotFindStatuses) {
+			JSONArray errors;
+			try {
+				errors = jsonObject.getJSONArray("errors");
+			} catch (JSONException ex) {
+				throw couldNotFindStatuses;
+			}
+			throw new JSONException(((JSONObject)errors.get(0)).getString("message"));
+		}
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < statuses.length(); i++) {
 			JSONObject status = statuses.getJSONObject(i);
